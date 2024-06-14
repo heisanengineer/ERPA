fe31095fd5fb082f62263f03e3ebeedfcf97f629
Emre Can Aşık <93338158+heisanengineer@users.noreply.github.com>
2024-06-14 18:23:22 +0300

    Basic Javascript DOM Examples

---
+++ Javascript/JavascriptDOM/JS/multiple-elements.js
@@ -0,0 +1,21 @@
+let sonuc;
+
+sonuc = document.getElementsByClassName("task")[0];
+
+taskList = document.getElementsByClassName("task");
+
+ul = document.getElementById("task-list2");
+taskList2 = ul.getElementsByTagName("li");
+
+taskList3 = ul.querySelectorAll("#task-list3 li");
+
+for (let i = 0; i < taskList.length; i++) {
+    console.log(taskList[i]);
+    
+}
+
+for(let task of taskList2){
+    task.style.color="red";
+    task.style.fontSize="20px";
+}
+console.log(sonuc);
---
+++ Javascript/JavascriptDOM/JS/single-elements.js
@@ -0,0 +1,21 @@
+let sonuc;
+
+sonuc = document.getElementById("title");
+sonuc = document.getElementById("title").id;
+sonuc = document.getElementById("title").className;
+sonuc = document.getElementById("title").classList;
+
+document.getElementById("title").style.fontSize="30px";
+document.getElementById("title").style.color="blue";
+
+document.getElementById("title").innerText = "Todo App";
+
+sonuc = document.querySelector("#title");
+sonuc = document.querySelector(".card-header");
+sonuc = document.querySelector("div");
+
+sonuc = document.querySelector("li");
+sonuc = document.querySelector("li:first-child");
+sonuc = document.querySelector("li:last-child");
+sonuc = document.querySelector("li:nth-child(2)");
+console.log(sonuc);
